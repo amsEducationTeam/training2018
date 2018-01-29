328859acabc4c20d66bf412bd0f5875fcafdb224
takimoto <h-takimoto@ams-inc.jp>
2018-01-29 17:48:47 +0900

    共有ユーザのコミット

--- src/sample01/SampleServlet.java
+++ src/sample01/SampleServlet.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package sample01;
 
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 
 /**
  * @author trainee
+ *
+ *共有した人が変更
  *
  */
 public class SampleServlet extends HttpServlet {
